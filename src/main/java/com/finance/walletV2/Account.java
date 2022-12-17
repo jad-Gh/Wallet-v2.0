bc98e64a146980409de6d1b18750b32c870973c9
jad-gh <jadghalayini1999@gmail.com>
2022-12-17 09:53:52 +0200

    Account started

---
+++ src/main/java/com/finance/walletV2/Account.java
@@ -0,0 +1,19 @@
+package com.finance.walletV2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+}
